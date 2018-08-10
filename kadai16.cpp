fd9405c621d63316eb6b90c72b73ed1bfda69fe1
urabe yuto <urabe.enjoyrugby@outlook.jp>
2018-08-10 13:16:53 +0900

    the third github

--- kadai16.cpp
+++ kadai16.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main(void){
     cout << "Hello World!!" << endl;
     cout << "Hello Bioinformatics!!" << endl;
-    cout << "Hello Urabe!!" << endl;
+    cout << "Hello Yuto!!" << endl;
     return 0;
 }
